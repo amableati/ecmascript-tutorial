1e34f856aba665f95918804bfc59312d671408fa
Atindra <kumaratindra77@gmail.com>
2022-12-13 00:20:01 +0530

    Practice Questions

---
+++ practice/sumOfArgs.js
@@ -0,0 +1,16 @@
+/*
+Create a function that takes two numbers as arguments and returns their sum.
+
+Examples
+addition(3, 2) ➞ 5
+addition(-3, -6) ➞ -9
+addition(7, 3, 3) ➞ 13
+addition(7, 3, 3, 1, 10, 4, 5) ➞ 33
+
+Notes
+Don't forget to return the result.
+*/
+
+function sumOfArgs(a, b) {
+	
+}
